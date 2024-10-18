a6902f3a444c42f006ab05b5a4f28c90fcbe8bd2
JLondo321 <londonovasquezjosemiguel@gmail.com>
2024-10-18 10:51:24 -0500

    Se realizan ajustes a la interfaz principal

--- proyecto_final/src/components/principal/Principal.jsx
+++ proyecto_final/src/components/principal/Principal.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import './Principal.css'; <<<<<<< Evelyn
+import './Principal.css';
 import Navbar from '../../botones/navbar/Navbar';
-import BotonDepositos from '../../botones/BotonDepositos/BotonDepositos';
-import BotonTransacciones from '../../botones/BotonTransacciones/BotonTransacciones';
-import BotonRetiros from '../../botones/BotonRetiros/BotonRetiros';
-import BotonPrestamos from '../../botones/BotonPrestamos/BotonPrestamos';
-import BotonInformes from '../../botones/BotonInformes/BotonInformes';
-import BotonLogOut from '../../botones/BotonLogOut/BotonLogOut';
 
 const Dashboard = () => {
   const user = {
@@ -18,7 +12,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <Navbar/>
-      
+
       <header className="dashboard-header">
         
         <div className="user-info">
@@ -34,14 +28,6 @@ const Dashboard = () => {
         <p className="balance-title">Depósito Bajo Monto</p>
         <h1 className="balance-amount">${user.balance.toLocaleString()}</h1>
         <p className="balance-total">Total ${user.balance.toLocaleString()}</p>
-      </div>
-      <div className="button-section">
-        <BotonDepositos />
-        <BotonTransacciones />
-        <BotonRetiros />
-        <BotonPrestamos />
-        <BotonInformes />
-        <BotonLogOut />  
       </div>
       <footer className="account-info">
         <p>Número de Cuenta: {user.accountNumber}</p>
